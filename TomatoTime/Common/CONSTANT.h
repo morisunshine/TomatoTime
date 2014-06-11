a7ac19ec66799aa58dc31c2a1e4c2b6796c7e0ab
morisunshine <sheldon.zen@gmail.com>
2014-06-11 13:54:33 +0800

    chore: add the CONSTANT represent glob var

---
+++ TomatoTime/Common/CONSTANT.h
@@ -0,0 +1,14 @@
+//
+//  CONSTANT.h
+//  TomatoTime
+//
+//  Created by Sheldon on 14-6-11.
+//  Copyright (c) 2014年 Sheldon. All rights reserved.
+//
+
+#ifndef TomatoTime_CONSTANT_h
+#define TomatoTime_CONSTANT_h
+
+
+
+#endif
